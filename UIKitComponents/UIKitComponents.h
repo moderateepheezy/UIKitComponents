225d85ca91dffd39cc4f067961b1f877d70d2b4b
Afees Lawal <moderateepheezy@gmail.com>
2019-09-05 14:15:10 +0100

    Initial Commit

---
+++ UIKitComponents/UIKitComponents.h
@@ -0,0 +1,19 @@
+//
+//  UIKitComponents.h
+//  UIKitComponents
+//
+//  Created by Afees Lawal on 05/09/2019.
+//  Copyright © 2019 Afees Lawal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIKitComponents.
+FOUNDATION_EXPORT double UIKitComponentsVersionNumber;
+
+//! Project version string for UIKitComponents.
+FOUNDATION_EXPORT const unsigned char UIKitComponentsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIKitComponents/PublicHeader.h>
+
+
